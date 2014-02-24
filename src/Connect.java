ebdacd16211723b8a70eaddf4e39ecfeddbad956
decam2009 <decam2009@gmail.com>
2014-02-24 22:01:51 +0600

    version 0.0001
    24.02.2013 22:01

--- src/Connect.java
+++ src/Connect.java
@@ -1,7 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
 /**
  * Created by BORIS on 24/02/14.
  */
 public class Connect
 {
+    public static HttpsURLConnection con;
 
+    public void Start()
+    {
+        String address = "https://dealer.telepayural.ru:8182/external/process/";
+        URL url;
+        try
+        {
+          url = new URL(address);
+          this.con = (HttpsURLConnection) url.openConnection();
+        }
+        catch (MalformedURLException mf)
+        {
+          mf.printStackTrace();
+        }
+        catch (IOException cn)
+        {
+          cn.printStackTrace();
+        }
+    }
+
+    public void test(HttpsURLConnection pcon) throws IOException
+    {
+      try
+      {
+        System.out.println("----------Content of the URL----------");
+        BufferedReader br = new BufferedReader(new InputStreamReader(pcon.getInputStream()));
+        String input;
+        while ((input = br.readLine()) != null)
+        {
+          System.out.println(input);
+        }
+        br.close();
+      }
+      catch(IOException e)
+      {
+        e.getStackTrace();
+      }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.io.IOException;
+
 /**
  * Created by BORIS on 23/02/14.
  */
 public class Main
 {
+    public static void main(String[] args)
+    {
+      Connect tpcon;
+      tpcon = new Connect();
+      tpcon.Start();
+      try
+      {
+        tpcon.test(Connect.con);
+      } catch (IOException e)
+      {
+        e.printStackTrace();
+      }
 
+    }
 }
